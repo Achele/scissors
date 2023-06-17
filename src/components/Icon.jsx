0b7c4685dd265af6d2c1cbf8f7308c964adbc06f
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-17 19:15:32 +0100

    update Icon component

--- src/components/Icon.jsx
+++ src/components/Icon.jsx
@@ -5,6 +5,11 @@ import slash from "../assets/Vector 3.png";
 import curves from "../assets/Vector (1).png";
 import lineThrough from "../assets/Vector (2).png";
 import arrow from "../assets/Line 7.png";
+import line from "../assets/Line 70.png";
+import grid from "../assets/grid.png";
+import linkIcon from "../assets/link-2.png";
+import edit from "../assets/edit.png";
+import activity from "../assets/activity.png";
 
 const LogoIcon = (props) => <img src={logo} alt=" Scissors logo" {...props} />;
 
@@ -25,4 +30,32 @@ const LineThrough = (props) => (
 );
 
 const Arrow = (props) => <img src={arrow} alt=" Scissors logo" {...props} />;
-export { LogoIcon, ChainIcon, LineIcon, Slash, Curves, LineThrough, Arrow };
+
+const Line = (props) => <img src={line} alt=" Scissors logo" {...props} />;
+
+const Grid = (props) => <img src={grid} alt=" Scissors logo" {...props} />;
+
+const LinkIcon = (props) => (
+  <img src={linkIcon} alt=" Scissors logo" {...props} />
+);
+
+const Edit = (props) => <img src={edit} alt=" Scissors logo" {...props} />;
+
+const Activity = (props) => (
+  <img src={activity} alt=" Scissors logo" {...props} />
+);
+
+export {
+  LogoIcon,
+  ChainIcon,
+  LineIcon,
+  Slash,
+  Curves,
+  LineThrough,
+  Arrow,
+  Line,
+  Grid,
+  LinkIcon,
+  Edit,
+  Activity,
+};
