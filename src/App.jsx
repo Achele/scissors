6fe473d15eb935c5814976d4fc22066531217f28
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-19 12:31:47 +0100

    Added features section

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import "./App.css";
 import Features from "./sections/Features";
 
 import Hero from "./sections/Hero";
+import Prices from "./sections/Prices";
 
 function App() {
   return (
     <>
       <Hero />
       <Features />
+      <Prices />
     </>
   );
 }
