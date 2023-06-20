71a6d8b9df17f5156d211d4998cb3829cc1924a8
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-20 12:55:33 +0100

    updated accordion functionality and logic

--- src/sections/Faq/Accordion.jsx
+++ src/sections/Faq/Accordion.jsx
@@ -1,5 +1,28 @@
-const Accordion = () => {
-  return <></>;
+import { useState } from "react";
+import { MinusIcon, PlusIcon } from "../../components/Icon";
+
+const Accordion = ({ title, content }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleAccordion = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <article className="bg-chat-gray-bg border rounded border-disabled-btn">
+      <button
+        className="w-full flex items-center justify-between p-3  focus:outline-none"
+        onClick={toggleAccordion}
+      >
+        <span className="font-medium">{title}</span>
+        {isOpen ? <MinusIcon /> : <PlusIcon />}
+      </button>
+      {isOpen && (
+        // <summary>
+        <p className="text-gray-700 flex items-center px-3 pb-3">{content}</p>
+        // </summary>
+      )}
+    </article>
+  );
 };
 
 export default Accordion;
