4bf02201fd67d4ff4d15f0b86ed160571bfb1721
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-20 12:56:40 +0100

    Updated faq with FaqData

--- src/sections/Faq/Faq.jsx
+++ src/sections/Faq/Faq.jsx
@@ -1,5 +1,14 @@
+import FaqData from "./FaqData";
+
 const Faq = () => {
-  return <></>;
+  return (
+    <main className="p-8">
+      <h1 className="py-4 text-lg">FAQS</h1>
+      <section>
+        <FaqData />
+      </section>
+    </main>
+  );
 };
 
 export default Faq;
