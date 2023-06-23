1c841e1431ab70d060da158f276fd40eb2484374
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-23 14:36:43 +0100

    style accordion

--- src/sections/Faq/Accordion.jsx
+++ src/sections/Faq/Accordion.jsx
@@ -8,17 +8,19 @@ const Accordion = ({ title, content }) => {
   };
 
   return (
-    <article className="bg-chat-gray-bg border rounded border-disabled-btn">
+    <article className="  border-b-2 rounded ">
       <button
         className="w-full flex items-center justify-between p-3  focus:outline-none"
         onClick={toggleAccordion}
       >
-        <span className="font-medium">{title}</span>
+        <span className="font-medium py-2">{title}</span>
         {isOpen ? <MinusIcon /> : <PlusIcon />}
       </button>
       {isOpen && (
         // <summary>
-        <p className="text-gray-700 flex items-center px-3 pb-3">{content}</p>
+        <p className="text-gray-700 flex items-center px-3 pb-3 text-sm">
+          {content}
+        </p>
         // </summary>
       )}
     </article>
