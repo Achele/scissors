eb476a42994f9e3d6bb33a84bb8cf8d588281419
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-23 14:37:10 +0100

    update sttyling for Faq section

--- src/sections/Faq/Faq.jsx
+++ src/sections/Faq/Faq.jsx
@@ -1,13 +1,18 @@
+import { Line } from "../../components/Icon";
 import FaqData from "./FaqData";
 
 const Faq = () => {
   return (
-    <main className="p-8">
-      <h1 className="py-4 text-lg">FAQS</h1>
-      <section>
-        <FaqData />
-      </section>
-    </main>
+    <section className="p-8 my-0 mx-auto w-3/4">
+      <span className="flex items-center justify-center">
+        <Line className={"h-6 px-3"} />
+        <h1 className="py-4 text-lg font-bold">FAQS</h1>
+      </span>
+
+      {/* <section> */}
+      <FaqData />
+      {/* </section> */}
+    </section>
   );
 };
 
