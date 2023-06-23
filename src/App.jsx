75de69b9a67671055627e481ec759e0f63ee901e
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-23 14:37:41 +0100

    Added font family

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import "./App.css";
 import Faq from "./sections/Faq/Faq";
 import Features from "./sections/Features";
+import Footer from "./sections/Footer";
+import GettingStarted from "./sections/GettingStarted";
 
 import Hero from "./sections/Hero";
 import Prices from "./sections/Prices";
 
 function App() {
   return (
-    <>
+    <div className="font-inter">
       <Hero />
       <Features />
       <Prices />
       <Faq />
-    </>
+      <GettingStarted />
+      <Footer />
+    </div>
   );
 }
 
