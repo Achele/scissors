527f15e80b1a19af67250b78b525fe3f9c9f29fe
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-23 14:36:19 +0100

    Add styling

--- src/sections/GettingStarted.jsx
+++ src/sections/GettingStarted.jsx
@@ -1,4 +1,17 @@
+import styles from "../styles/Hero.module.css";
+
 const GettingStarted = () => {
-  return <></>;
+  return (
+    <section
+      className={`${styles.bckgroundGradient} flex flex-col justify-center items-center p-8 my-8`}
+    >
+      <h1 className="text-white font-semibold text-center py-4 text-4xl">
+        Revolutionizing Link Optimization
+      </h1>
+      <button className="rounded-full bg-primary py-1 text-white px-8 text-lg">
+        Get Started
+      </button>
+    </section>
+  );
 };
 export default GettingStarted;
