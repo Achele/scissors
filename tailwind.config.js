a58ed8d67596838840653e93046c5e8922e72e06
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-23 14:38:26 +0100

    Update config with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+      },
+      colors: {
+        primary: "#0065FE",
+        primaryBlue: "#F9FBFD",
+        lightBlue: " #3284FF21",
+        blueGradient:
+          " linear-gradient(180deg, #1E3448 99.99%, rgba(30, 52, 72, 0) 100%)",
+      },
+    },
   },
   plugins: [],
 };
