05f131f4d4e6ce77ccf3d7243d95db5a7c1b8cd3
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-29 01:53:14 +0100

    updated the routes with AuthContextProvider and Protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,29 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Account from "./pages/Account";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { AuthContextProvider } from "./context/authContext";
+import ProctedRoutes from "./components/ProtectedRoutes";
 
 function App() {
   return (
     <div className="font-inter">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/account" element={<Account />} />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route
+            path="/account"
+            element={
+              <ProctedRoutes>
+                <Account />
+              </ProctedRoutes>
+            }
+          />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </AuthContextProvider>
     </div>
   );
 }
