0ed23db49e05965b02de3ed26b3233ce0d595fc0
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-29 01:51:33 +0100

    created dummy data for link card

---
+++ src/utils/data.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    id: "54dfghju",
+    createdAt: new Date(),
+    name: "My website",
+    longURL: "https://google.com",
+    shortCode: "masdo",
+    totalClicks: 313,
+  },
+  {
+    id: "524dfghju",
+    createdAt: new Date(),
+    name: "My website",
+    longURL: "https://google.com",
+    shortCode: "masdo",
+    totalClicks: 613,
+  },
+];
