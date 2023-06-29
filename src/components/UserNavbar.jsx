56a56c06f7edf28c185b0a715acc1da85d53611a
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-06-29 01:50:33 +0100

    Add log out logic

--- src/components/UserNavbar.jsx
+++ src/components/UserNavbar.jsx
@@ -1,36 +1,36 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
+import { UseUserAuth } from "../context/authContext";
 // import { ChainIcon, LineIcon, LogoIcon } from "./Icon";
 
 const UserNavbar = () => {
+  const { user, logout } = UseUserAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await logout();
+      navigate("/");
+      console.log(`${user.email} logged out`);
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
   return (
     <nav className="flex p-3 items-center justify-between">
-      {/* <div className="flex items-center ">
-        <ChainIcon className={"w-5 "} />
-        <LineIcon className={"h-5 px-2"} />
-        <LogoIcon className={"w-12"} />
-      </div> */}
       <div>
         <NavLink to={"/urls"} className={"text-primary px-1"}>
           My URLs
         </NavLink>
-        {/* <NavLink to={"/features"} className={"px-4"}>
-          Features
-        </NavLink>
-        <NavLink to={"/pricing"} className={""}>
-          Pricing
-        </NavLink>
-        <NavLink to={"/analytics"} className={"px-4"}>
-          Analytics
-        </NavLink>
-        <NavLink to={"/faqs"}>FAQs</NavLink> */}
       </div>
       <div>
-        <NavLink to={"/logout"} className={"text-primary text-base pr-1"}>
+        <h1>{user.displayName}</h1>
+        <button
+          className={"text-primary text-base pr-1"}
+          onClick={handleLogout}
+        >
           Log out
-        </NavLink>
-        {/* <button className="rounded-full bg-primary text-white py-2 px-4 text-xs ml-2">
-          Try for free
-        </button> */}
+        </button>
       </div>
     </nav>
   );
