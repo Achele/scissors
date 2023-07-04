cf222fd7367a16c194566db6e0866c9e131ece23
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-04 23:33:31 +0100

    Create popup modal for links creation

---
+++ src/components/Modal.jsx
@@ -0,0 +1,67 @@
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import { CloseIcon } from "./Icon";
+import FormikControl from "./FormikControl";
+
+const Modal = ({ setShowPopup, createShortenLink }) => {
+  const initialValues = {
+    name: "",
+    longURL: "",
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string().required("Required"),
+    longURL: Yup.string().required("Required"),
+  });
+
+  const onSubmit = (values) => {
+    setShowPopup(false);
+    createShortenLink(values.name, values.longURL);
+    console.log("FORM DATA: ", values);
+  };
+  return (
+    <>
+      <div className="fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-auto sm:w-1/2 lg:max-w-2xl bg-white p-8 rounded-md shadow-md z-50">
+        <span className="flex items-center justify-between">
+          <h2 className="text-xl font-bold mb-4 pl-14 lg:pl-24">Shorten URL</h2>
+          <CloseIcon onClick={() => setShowPopup(false)} />
+        </span>
+
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {(formik) => {
+            return (
+              <Form className="container w-2/3 mx-auto">
+                <FormikControl
+                  control="input"
+                  type="text"
+                  placeholder="Name"
+                  name="name"
+                />
+
+                <FormikControl
+                  control="input"
+                  type="text"
+                  placeholder="Long URL"
+                  name="longURL"
+                />
+                <button
+                  type="submit"
+                  disabled={!formik.isValid}
+                  className="bg-primary text-white px-4 rounded py-1 my-3 ml-40 text-sm"
+                >
+                  Shorten URL
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
+      </div>
+    </>
+  );
+};
+
+export default Modal;
