80c988eb340f4c1575a12e2834ab7af4f2a7c32c
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-06 22:07:29 +0100

    Fetch links data from the database

--- src/components/linkRedirect/index.jsx
+++ src/components/linkRedirect/index.jsx
@@ -1,8 +1,24 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { db } from "../../firebase/config";
 
 const LinkRedirect = () => {
-  const params = useParams();
-  return <div>{JSON.stringify(params, null, 2)}</div>;
+  const { shortCode } = useParams();
+
+  useEffect(() => {
+    const fetchLinkDoc = async () => {
+      const linkRef = doc(db, "links", shortCode);
+      const linkDoc = await getDoc(linkRef);
+      if (linkDoc.exists) {
+        const { longURL } = linkDoc.data();
+        window.location.href = longURL;
+      }
+    };
+    fetchLinkDoc();
+  }, []);
+
+  return <div>link redirect</div>;
 };
 
 export default LinkRedirect;
