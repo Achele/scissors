f9e19a1bde20a88bce21d53e79615ad7053e844d
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-06 18:33:46 +0100

    Add copy functionality to shortened link

--- src/components/LinkCard.jsx
+++ src/components/LinkCard.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { useMemo, useState } from "react";
 // import { data } from "../utils/data";
 import { Analytics } from "./Icon";
 import format from "date-fns/format";
@@ -6,12 +7,21 @@ import format from "date-fns/format";
 const LinkCard = ({
   id,
   createdAt,
-  name,
+  citeName,
   longURL,
   shortCode,
   totalClicks,
   isLastItem,
+  deleteLink,
+  copyLink,
 }) => {
+  console.log("link card rendered");
+  const formattedDate =
+    createdAt && createdAt.seconds
+      ? format(new Date(createdAt.seconds * 1000), "MMM dd, yyyy")
+      : "-";
+  const shortUrl = `${window.location.host}/${shortCode}`;
+
   return (
     <article
       className={`flex items-center justify-between mb-4 ${
@@ -20,16 +30,24 @@ const LinkCard = ({
     >
       <span>
         <p className="text-placeholder uppercase py-2 font-bold">
-          Created at {format(createdAt, "d MMM, HH:mm")}
+          Created at {formattedDate}
         </p>
-        <h1 className="font-bold text-base py-2">{name}</h1>
+        <h1 className="font-bold text-base py-2">{citeName}</h1>
         <Link>{longURL}</Link>
         <span className="flex items-center py-6">
-          <p className="pr-5 text-primary">
-            {window.location.host}/{shortCode}
-          </p>
-          <button className="border-primary border text-primary rounded px-2">
+          <p className="pr-5 text-primary">{shortUrl}</p>
+          <button
+            className="border-primary border text-primary rounded px-2 py-1"
+            onClick={() => copyLink(shortUrl)}
+          >
             Copy
+          </button>
+
+          <button
+            className="text-white rounded py-1 px-2 bg-gray-950 border mx-2"
+            onClick={deleteLink}
+          >
+            Delete
           </button>
         </span>
       </span>
@@ -38,7 +56,6 @@ const LinkCard = ({
           <h3 className="font-bold">{totalClicks}</h3>
           <Analytics />
         </span>
-
         <p>total clicks</p>
       </span>
     </article>
