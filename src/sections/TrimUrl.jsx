fc41b931f733684699bff78ebfb16fec8665a8e3
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-08 11:48:40 +0100

    update trim url

--- src/sections/TrimUrl.jsx
+++ src/sections/TrimUrl.jsx
@@ -1,20 +1,73 @@
+import { Form, Formik } from "formik";
+import * as Yup from "yup";
+import { useNavigate } from "react-router-dom";
+import FormikControl from "../components/FormikControl";
+
 const TrimUrl = () => {
+  const dropDownOptions = [
+    { key: "Enter Domain", value: "" },
+    { key: "Scissors.com", value: "scissors.com" },
+    { key: "Add Domain", value: "Add Domain" },
+  ];
+
+  const navigate = useNavigate();
+
+  const initialValues = {
+    url: "",
+    selectDomain: "",
+    alias: "",
+  };
+
+  const validationSchema = Yup.object({
+    url: Yup.string().required("Required"),
+    selectDomain: Yup.string().required("Required"),
+    alias: Yup.string(),
+  });
+
+  const onSubmit = (values) => {
+    console.log("FORM DATA: ", values);
+    navigate("/signup");
+  };
+
   return (
     <section>
       <div>
-        <form>
-          <input type="text" placeholder="Paste URL here..." />
-          <select name="" id="">
-            <optgroup>
-              <option value="">Enter Domain</option>
-              <option value="scissors.com">Scissors.com</option>
-              <option value="Add Domain">Add Domain</option>
-            </optgroup>
-          </select>
-          <input type="text" placeholder="Type alias here" />
-          <button>Trim URL</button>
-        </form>
-        <p>
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {(formik) => {
+            return (
+              <Form>
+                <FormikControl
+                  control="input"
+                  placeholder="Paste URL here..."
+                  name="url"
+                />
+                <span>
+                  <FormikControl
+                    control="select"
+                    name="selectDomain"
+                    options={dropDownOptions}
+                  />
+                  <FormikControl
+                    control="input"
+                    placeholder="Type alias here"
+                    name="alias"
+                  />
+                </span>
+                <button
+                  className="bg-primary border rounded py-2 px-4 text-white"
+                  disabled={!formik.values}
+                >
+                  Trim URL
+                </button>
+              </Form>
+            );
+          }}
+        </Formik>
+        <p className="text-primary">
           By clicking TrimURL, I agree to the Terms of Service, Privacy Policy
           and Use of Cookies.
         </p>
