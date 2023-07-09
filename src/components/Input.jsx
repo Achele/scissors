a68cc94bd6928c1414f1f7962a1b361db51cfcea
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-09 19:28:00 +0100

    Update styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -11,13 +11,14 @@ const Input = (props) => {
       >
         {label}
       </label>
+
       <Field
         className="border-primary rounded-md border py-2 px-4 w-full placeholder-placeholder text-xs md:text-sm mt-4"
-        // className="w-full px-4 py-2 pr-10 text-xs md:text-sm rounded-md bg-white shadow-md border-border-input placeholder-gray-500"
         id={name}
         name={name}
         {...rest}
       />
+
       <ErrorMessage name={name} component={TextError} />
     </div>
   );
