2b22e115267a3cf9f0e07498fc594a621fa6a2e4
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-09 19:31:30 +0100

    Added screen sizw for smaller screens

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,12 +6,18 @@ export default {
       fontFamily: {
         inter: ["Inter", "sans-serif"],
       },
+      screens: {
+        xs: "279px",
+        // => @media (min-width: 279px) {...}
+      },
       colors: {
         primary: "#0065FE",
         primaryBlue: "#F9FBFD",
         lightBlue: " #3284FF21",
         placeholder: "#A0B1C0",
         anotherBlue: " #4991FF",
+        darkBlue: "#0D1C2B",
+        navText: "#A3C8FF",
       },
     },
   },
