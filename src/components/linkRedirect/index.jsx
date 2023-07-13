6f8ada424246cad33f1544f958edda28266b9ec6
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-07-13 02:35:52 +0100

    Style redirect page

--- src/components/linkRedirect/index.jsx
+++ src/components/linkRedirect/index.jsx
@@ -1,4 +1,4 @@
-import { doc, getDoc, updateDoc, runTransaction } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { db } from "../../firebase/config";
@@ -14,11 +14,12 @@ const LinkRedirect = () => {
     const fetchLinkDoc = async () => {
       const linkRef = doc(db, "links", shortCode);
       const linkDoc = await getDoc(linkRef);
+
       if (linkDoc.exists()) {
         const { longURL, linkID, userUid } = linkDoc.data();
         const userLinksRef = doc(db, "users", userUid, "links", linkID);
         await updateDoc(userLinksRef, {
-          totalClicks: increment(1), // Increment the totalClicks value by 1
+          totalClicks: increment(1),
         });
         window.location.href = longURL;
         setIsValidLink(true);
@@ -28,13 +29,18 @@ const LinkRedirect = () => {
       setLoading(false);
     };
     fetchLinkDoc();
-  }, []);
+  }, [shortCode]);
   return (
-    <div>
+    <div className="w-1/2 my-12 mx-auto">
       {loading ? (
         <Loading />
       ) : isValidLink ? (
-        <h1>Redirecting to the link</h1>
+        <span className="">
+          <Loading />
+          <h1 className="text-center font-extrabold text-primary">
+            Redirecting to the link....
+          </h1>
+        </span>
       ) : (
         <h1 className="w-1/2 my-4 mx-auto text-primary">Link not valid</h1>
       )}
