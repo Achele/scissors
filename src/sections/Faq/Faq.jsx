dec4293cbfb7e9fa990b7f51751bdb76b043f8c1
Ojoachele Onuh <ejigboojoachele@gmail.com>
2023-08-02 18:03:10 +0100

    update secion id

--- src/sections/Faq/Faq.jsx
+++ src/sections/Faq/Faq.jsx
@@ -3,7 +3,7 @@ import FaqData from "./FaqData";
 
 const Faq = () => {
   return (
-    <section className="p-8 my-0 mx-auto w-3/4">
+    <section className="p-8 my-0 mx-auto w-3/4" id="faqs">
       <span className="flex items-center justify-center">
         <Line className={"h-6 px-3"} />
         <h1 className="py-4 text-lg font-bold">FAQS</h1>
